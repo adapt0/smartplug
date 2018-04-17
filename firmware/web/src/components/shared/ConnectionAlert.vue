b6aecd78e9ae64c4da839dd1f3b27dfcfee41161
Adaptation <adapt@adapt.gen.nz>
2018-04-16 19:10:00 -0700

    web: tweak ConnectionAlert layout

--- firmware/web/src/components/shared/ConnectionAlert.vue
+++ firmware/web/src/components/shared/ConnectionAlert.vue
@@ -4,14 +4,27 @@ Licensed under the MIT License. Refer to LICENSE file in the project root.
 -->
 <template>
   <div class="connection alert d-flex justify-content-between" v-bind:class="[ disconnected ? 'alert-danger' : 'alert-success', { active: disconnected }]" role="alert">
-    <template v-if="disconnected">
-      <div>Connection lost. Attempting to reconnect...</div>
-      <div class="pl-4"><icon name="spinner" pulse /></div>
-    </template>
-    <template v-else>
-      <div>Connection reestablished!</div>
-    </template>
-  </div>
+    <div class="container">
+      <div class="row">
+        <template v-if="disconnected">
+          <div class="col">&nbsp;</div>
+          <div class="col-md-auto text-center">
+            Connection lost. Attempting to reconnect...
+          </div>
+          <div class="col text-right">
+            <icon class="align-text-bottom" name="spinner" pulse />
+          </div>
+        </template>
+        <template v-else>
+          <div class="col-sm">&nbsp;</div>
+          <div class="col-md-auto text-center">
+            Connection reestablished!
+          </div>
+          <div class="col-sm">&nbsp;</div>
+        </template>
+      </div>
+    </div>
+ </div>
 </template>
 
 <script>
@@ -41,7 +54,7 @@ export default {
 
   border-top-left-radius: 0;
   border-top-right-radius: 0;
-  text-align: center;
+  margin: 0;
   z-index: 1000;
 
   pointer-events: none;
